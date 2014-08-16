26695f8ea2dd85e4cfdaa46d1c9e356e468579c7
John Sullivan <josullivan@mozilla.com>
2014-08-15 21:26:04 -0700

    Added manual tests for a few things.

---
+++ test_dlogger.py
@@ -0,0 +1,30 @@
+import logging
+import dlogger
+
+def setup_logging():
+    logger = logging.getLogger("test_dlogger")
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(dlogger.DifferentFormatter())
+    logger.addHandler(handler)
+
+    return logger
+
+
+def main():
+    logger = setup_logging()
+
+    levels = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"]
+    for i in levels:
+        logger.log(getattr(logging, i), "This is a {0} message.", i)
+
+    for i in levels:
+        try:
+            raise RuntimeError("This is a description of the exception.")
+        except RuntimeError:
+            logger.log(getattr(logging, i), "This is a {0} exception message", i, exc_info=True)
+
+
+if __name__ == "__main__":
+    main()
