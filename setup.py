9c0ed8e5136d70742204320e8c041f56ecb6d190
John Sullivan <johnsullivan.pem@gmail.com>
2018-11-08 11:25:56 -0800

    Added setup.py file.
    I wanted to use this silly package for an equally silly personal
    project, and wanted to be able to point `pip` at dlogger's GitHub
    repo.
    
    If you'd like me to publish this to PyPI, please let me know, I'll
    get a kick out of it.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name="dlogger",
+	version="0.1",
+	description="A different logger for Python",
+	author="John Sullivan",
+	author_email="johnsullivan.pem@gmail.com",
+	url="https://github.com/brownhead/different-logger",
+	packages=["dlogger"],
+)
